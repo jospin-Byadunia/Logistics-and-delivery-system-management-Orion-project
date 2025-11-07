00ebd2dbca0bd9fa70cb8cecb657b67eaf6ba763
jospin Byadunia <jmurhiorhakube@gmail.com>
2025-11-07 22:33:09 +0200

    clear

--- api/serializers.py
+++ api/serializers.py
@@ -62,11 +62,6 @@ class DeliveryRequestSerializer(serializers.ModelSerializer):
             'distance_km', 'price', 'status', 'created_at', 'updated_at','is_paid'
         ]
         read_only_fields = ['id', 'distance_km', 'price', 'created_at', 'updated_at', 'customer']
-    # def create(self, validated_data):
-    #     request = self.context.get('request')
-    #     if request and hasattr(request, 'user'):
-    #         validated_data['customer'] = request.user
-    #     return super().create(validated_data)
 
 
 # --------------------
